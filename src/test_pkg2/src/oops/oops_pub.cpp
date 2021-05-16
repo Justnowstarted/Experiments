24326a780cb30498ee5aeab0c4d856364ea5708a
Justnowstarted <pt.procoders@gmail.com>
2021-05-16 00:28:41 -0700

    oops added

--- src/test_pkg2/src/oops/oops_pub.cpp
+++ src/test_pkg2/src/oops/oops_pub.cpp
@@ -7,67 +7,34 @@ class Publish_{
     private:
         int counter{0};
     public:
-        Publish_(ros::Nodehandle n);
-        ~Publish_();
-};#include "ros/ros.h"
-#include "std_msgs/String.h"
+        Publish_(ros::NodeHandle* nh)
+        {
+            ros::Publisher pub = nh->advertise<std_msgs::String>("/counter", 100);
+            ros::Rate loop_rate(10);
+            while (ros::ok())
+            {
+                std_msgs::String msg;
+                msg.data = "Hey Pavan, I am publishing from here";
+                pub.publish(msg);
+                ros::spinOnce();
 
-#include<sstream>
-
-class Publish_{
-    private:
-        int counter{0};
-    public:
-        Publish_(ros::Nodehandle n);
+                loop_rate.sleep();
+            }
+            
+        }
         ~Publish_();
 };
 
-Publish_::Publish_(ros::Nodehandle n)
-{
-    ros::Publisher pub = nh.advertise<std_msgs::String>("/counter", 100);
-    ros::Rate loop_rate(10);
-    while (ros::ok())
-    {
-        std_msgs::String msg;
-        msg.data = "Hey Pavan, I am publishing from here";
-        pub.publish(msg)
-        ros::spinOnce();
 
-        loop_rate.sleep();
-    }
-    
-}
 
 Publish_::~Publish_()
 {
 }
 
 
-int main(int argc, char** argv){
-    ros::init(argc, argv, "oops_talker");
-    ros::Nodehandle nh;
-    Publish_ p = Publish_(&nh)
-}
-
-Publish_::Publish_(ros::Nodehandle n)
-{
-    ros::Publisher pub = nh.advertise<std_msgs::String>("/counter", 100);
-    while (ros::ok())
-    {
-        std_msgs::String msg;
-        msg.data = "Hey Pavan, I am publishing from here";
-        pub.publish(msg)
-    }
-    
-}
-
-Publish_::~Publish_()
-{
-}
-
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "oops_talker");
-    ros::Nodehandle nh;
-    Publish_ p = Publish_(&nh)
+    ros::NodeHandle nh;
+    Publish_ p = Publish_(&nh);
 }
