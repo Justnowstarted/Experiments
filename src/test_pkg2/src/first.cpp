01699dedfd98d27d80f551b33741f547048b2fe4
Pavan Kumar <kavvuripavankumar@gmail.com>
2021-07-10 19:56:27 +0100

    Why CMake?

--- src/test_pkg2/src/first.cpp
+++
@@ -1,9 +0,0 @@
-#include "ros/ros.h"
-
-#include "test_pkg2/chararray.h"
-#include "pkg3_msgs/floatarray.h"
-
-int main(int argc, char** argv){
-    ros::init(argc, argv, "node1");
-    return 0;
-}
--- src/test_pkg2/src/first_listen.cpp
+++
@@ -1,7 +0,0 @@
-#include "ros/ros.h"
-
-int main(int argc, char** argv){
-    ros::init(argc, argv, "node1_listen");
-
-    return 0;
-}
